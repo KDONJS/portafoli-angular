5f2b4e833c820e8ad05681fa115aabd8c2c04150
KDONJS <yorlinssj@hotmail.com>
2023-09-10 19:02:30 -0500

    commit de mejora visual

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  elementos = ['Elemento 1', 'Elemento 2', 'Elemento 3', 'Elemento 4'];
+  elementos = ['Elemento 1', 'Elemento 2', 'Elemento 3', 'Elemento 4','Elemento 1', 'Elemento 2', 'Elemento 3', 'Elemento 4'];
 }
 
